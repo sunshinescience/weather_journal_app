89e98dd01f115b69a34130b9c20aac2f53a29d1f
Sunshine Abbott <sunshine.abbott@gmail.com>
2020-03-31 05:45:00 +0100

    set up the server in server.js file

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Require Express to run server and routes
+// Require Express (which we've already installed on the command line) to run server and routes
 const express = require('express');
 
-// Start up an instance of app
+// Create an instance of our app, with express
 const app = express();
 
 // Dependencies
 const bodyParser = require('body-parser');
 
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// Here we are configuring express to use body-parser as middle-ware so that we can parse our data
+app.use(bodyParser.urlencoded({ extended: false })); // Here we use the 'use' method to tell bodyParser exactly how we want our data to be dealt with
+app.use(bodyParser.json()); // We're going to mostly want JSON
 
 // Cors for cross origin allowance
-const cors = require('cors');
+const cors = require('cors'); // Require Cors (which we've already installed on the command line) which let's the browser and server talk to each other withour any security interruptions
 app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static('website')); // We use our 'use' method and this time, we're pointing our app to the folder that we want it to look at
 
 // Setup Server
+const port = 3000; // We set our port
+
+const server = app.listen(port, listening); // Call the listen method and pass it our callback function
+
+function listening() {
+    console.log('server running');
+	console.log(`server running on local host: ${port}`);
+}
+
